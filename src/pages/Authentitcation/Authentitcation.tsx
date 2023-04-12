e40fbb5a103b1d3d2bcf9fcd821cf8d59d108104
marcela_llanos <marcela3llanos@gmail.com>
2023-04-12 18:33:47 -0400

    Add of MUI to the project

---
+++ src/pages/Authentitcation/Authentitcation.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './styles/Authentitcation.css';
+export interface AuthentitcationProps {}
+
+const Authentitcation : React.FC<AuthentitcationProps> = () => {
+	return <div className='authentitcation'>Authentitcation</div >;
+};
+
+export default Authentitcation;
